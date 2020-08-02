13d6e6c8780effda41c808f14dcd1a7722f2ae72
Ryan Kohanski <RyanLKohanski@gmail.com>
2020-08-01 20:05:49 -0500

    Feedback

---
+++ plotlydash-flask-tutorial-master/plotlydash_flask_tutorial/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class FeedbackForm(FlaskForm):
+    """Feedback form."""
+    body = TextField('', [
+        DataRequired(),
+        Length(min=1, message=('Your message is too short.'))])
+    submit = SubmitField('Submit')
